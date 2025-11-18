01c5258cd918f389618f9ca5ac2c586f0790cf39
Zidane Khan <ZK329@live.mdx.ac.uk>
2025-11-18 13:27:08 +0400

    Commit streamlit --> week-08 Workshop

---
+++ layout-demo.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.set_page_config(page_title="Layout Demo", layout="wide")
+st.title("Layout Demo -- Building a Simple Dashboard")
+
+#Side-bar
+
+with st.sidebar:
+    st.header('Controls')
+    points = st.slider('No. Points', 10, 500, 100)
+    table = st.checkbox('Raw data')
+
+#Main
+st.header('Main content')
+data = pd.DataFrame(
+    np.random.randn(points, 3),
+    columns = ['Feature 1', 'Feature 2', 'Feature 3']
+)
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header('Line Chart')
+    st.line_chart(data)
+
+with col2:
+    st.header('Bar Chart')
+    st.bar_chart(data)
+
+with st.expander('Raw data'):
+    if table:
+        st.dataframe(data)
+    else:
+        st.info('--')
---
+++ readme.md
@@ -0,0 +1,3 @@
+In this workshop, we are learning streamlit framework.
+Creating applicstions without front end stack (HTML, CSS, JS)
+Useful for analytics (use for Week 12 Assignment)
